e267ce8519ae2cf58b1a95e57c6b3d0d7b03a633
D3fau4 <raulwii2@gmail.com>
2018-08-15 20:22:44 +0200

    Update UI

--- Source/UI/Source/Main.cpp
+++ Source/UI/Source/Main.cpp
@@ -3,27 +3,28 @@
 #include "CustomUI.h"
 
 // Create two pages with their names
-static CustomUI::Page page1("Page 1");
-static CustomUI::Page page2("Page 2");
+static CustomUI::Page page1("Main");
+static CustomUI::Page page2("Homebrew");
+static CustomUI::Page page3("About");
 
 // Static var
-static string text1 = "Sample";
+static string text1 = "Menu";
 
 // Rendering functions
 void page1render()
 {
-    page1.renderText(text1, 20, 20, { 0, 0, 255, 255 }, 50);
+    page1.renderText(text1", 20, 20, { 0, 0, 255, 255 }, 50);
 }
 
 void page2render()
 {
-    page1.renderText("Text 2!", 30, 30, { 255, 0, 255, 255 }, 50);
+    page1.renderText("WIP", 30, 30, { 255, 0, 255, 255 }, 50);
 }
 
 int main()
 {
     // Init libs, with a title, a footer and a theme (you can create your own themes!)
-    CustomUI::init("Sample title", "Test footer", CustomUI::HorizonDark());
+    CustomUI::init("Punto Scene Menu", "Alpha", CustomUI::HorizonDark());
 
     // Set their rendering functions (what will be called every time the UI renders)
     page1.onRender(page1render);
@@ -32,6 +33,7 @@ int main()
     // Add both pages to our UI
     CustomUI::addPage(page1);
     CustomUI::addPage(page2);
+	CustomUI::addPage(page3);
 
     // Main loop starts
     while(appletMainLoop())
