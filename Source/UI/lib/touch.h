e380d4e3931799de5a2b65af50a16f4c0964d14b
D3fau4 <raulwii2@gmail.com>
2018-08-15 19:56:49 +0200

    Change UI

--- Source/UI/lib/touch.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "precisionui.h"
-
-bool PUI_Touch_Touching(u32 touch_count, int x1, int y1, int x2, int y2);
-int PUI_Touch_Sidebar_Touching(u32 touch_count);
-int PUI_Touch_Element_Touching(u32 touch_count);
